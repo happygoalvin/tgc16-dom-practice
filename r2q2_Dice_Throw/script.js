5ff5c52b418a4fb94dbc1dc0ccfd8ec2d8c931d8
happygoalvin <yo.alvin.96@gmail.com>
2022-02-09 19:16:42 +0800

    completed r2q2

--- r2q2_Dice_Throw/script.js
+++ r2q2_Dice_Throw/script.js
@@ -1,3 +1,19 @@
 // PLACE YOUR CODE HERE
+let roll = document.querySelector('#btn');
+roll.addEventListener('click', function(){
+    // generate a random number between 1 to 6
+   let randomNumber = Math.floor(Math.random() * 6 + 1);
+
+   // 1. select the container that we want add into
+   let p = document.querySelectorAll('.box');   
+
+   // 2. Append in the new random number
+   p[0].innerHTML = randomNumber
+   p[1].innerHTML = randomNumber
+
+   let result = document.querySelector('#result');
+   result.innerText = parseFloat(p[0].innerHTML) + parseFloat(p[1].innerHTML);
+})
+
 
 
