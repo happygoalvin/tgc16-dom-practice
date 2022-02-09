72db46a6662daca9ae42534bacf83e3d330ab03c
happygoalvin <yo.alvin.96@gmail.com>
2022-02-09 17:51:21 +0800

    Completed up till r2q1

--- r2q1_Rounding_up_the_criminals/script.js
+++ r2q1_Rounding_up_the_criminals/script.js
@@ -1,3 +1,14 @@
 // PLACE YOUR CODE HERE
+let a = document.querySelectorAll('div.box.a')
+for (let arrested of a) {
+ arrested.style.backgroundColor = "red";
+ let taskElement = document.createElement("span")
+ taskElement.innerHTML = "jailed"
+ arrested.appendChild(taskElement)
+}
 
+let u = document.querySelectorAll('div.box.a.undercover')
+for (let undercover of u){
+undercover.style.backgroundColor = "yellow";
+}
 
